f976a0562f0362fa55fde4ae1cf07664af60c201
Alex Crooks <alexcrooks2@gmail.com>
2025-12-14 13:44:05 -0800

    Ball rolling on prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import SignIn from "@/components/signin";
 import SignOut from "@/components/signout";
 import { auth } from "@/auth";
+import prisma from "@/lib/prisma";
 
 export default async function Home() {
   const session = await auth();
-  console.log("Session:", session);
-  const userName = session?.user ? `${session?.user?.name}'s `: "";
+  // console.log("Session:", session);
+  let user = null;
+
+  if (session) {
+    user = await prisma.user.findUnique({
+      where: {
+        id: session.user?.id,
+      }
+    });
+  }
+  const userName = session?.user ? `${ session?.user?.name}'s `: "";
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans">
       <main>
@@ -13,7 +23,14 @@ export default async function Home() {
         {!session && (
           <SignIn />)}
         {session && (
-          <SignOut />)}
+          <>
+          <div className="bg-neutral-900 rounded p-3">
+              <pre className="text-xs text-gray-300">
+                {JSON.stringify(user, null, 2)}
+              </pre>
+            </div>
+          <SignOut />
+          </>)}
       </main>
     </div>
   );
